93ccdb8878140202f5b16bd9e9336fc95363e335
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-02 16:15:44 +0530

    Install tailwind css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello from Ranjeet Singh</h1>
+      <h1 className="text-5xl">Hello from Ranjeet Singh</h1>
     </div>
   );
 }
