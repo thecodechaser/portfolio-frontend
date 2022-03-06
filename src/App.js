9d4a1c043b18e1ac5335667f159c52573e892632
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-06 13:49:09 +0530

    Add markup for header

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App" >
+    <div className="App">
       <Header />
-      </div>
+    </div>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React from 'react';
 
-const Header =()=>{
-    return (
-
-    );
-}
+const Header = () => (
+  <header className="flex justify-between px-20 py-7 shadow-xl">
+    <div><a href="/">TheCodeChaser</a></div>
+    <div>
+      <ul className="flex gap-5">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">Portfolio</a>
+        </li>
+        <li>
+          <a href="/">Skills</a>
+        </li>
+        <li>
+          <a href="/">Blog</a>
+        </li>
+      </ul>
+    </div>
+  </header>
+);
 
 export default Header;
