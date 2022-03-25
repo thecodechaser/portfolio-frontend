aadc2aec9b3ec0be818aa52dbdc1789f4251181e
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-25 12:01:11 +0530

    Add boilerplate for contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Contact = () => (
-  <div />
+   <div className="flex flex-col ml-4 items-center mt-20">
+     <div className="flex w-full justify-center">
+     <div className="border-b-2 mb-3 w-32 mr-3 about-hr md:mr-5 border-secondaryColor " />
+     <h3 className="text-2xl md:text-4xl">Get in touch</h3>
+     <div className="border-b-2 mb-3 w-32  ml-3 about-hr md:ml-5 border-secondaryColor" />
+     </div>
+   </div>
 );
 
 export default Contact;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="flex justify-center my-5">
-    <p>
+    <p className="text-md">
       Build by
       <a href="https://github.com/thecodechaser" className="text-secondaryColor"> Ranjeet Singh </a>
       © 2022
