1c9af3fe0092542f5aabccbef6f501b026ff911f
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-22 18:09:48 +0530

    Add more exprience

--- src/components/About/Experience.js
+++ src/components/About/Experience.js
@@ -1,4 +1,5 @@
 import microverseLogo from '../../assets/microverse_logo.png';
+import freelanceLogo from '../../assets/freelance-logo.jpg';
 import { ChevronRightIcon } from '@heroicons/react/solid';
 
 const Experience = () => {
@@ -9,10 +10,10 @@ const Experience = () => {
       <div className="border-b-2 w-3/5 mb-3 border-secondaryColor hr-about" />
     </div>
 
-    <div>
+    <div className="mt-8">
       <div className="flex mt-4 text-lg text-secondaryColor ml-5 gap-16 md:gap-96 md:ml-0">
       <h3>Technical Support Enginner <span className="block text-sm ml-1 text-skyColor">January 2022 – Present</span></h3>
-      <p><img src={microverseLogo} alt="microverse-logo" className="inline w-8"/> Microverse</p>
+      <p><img src={microverseLogo} alt="microverse-logo" className="inline w-8"/> Microverse<span className="block text-sm text-skyColor">Remote</span></p>
       </div>
       <ul className="ml-4 md:ml-0">
       <li className="flex text-base gap-2 text-skyColor mt-4">
@@ -22,6 +23,40 @@ const Experience = () => {
               <li className="flex text-base gap-2 text-skyColor mt-2">
                 <ChevronRightIcon className="h-6" />
                 <p>Proposed improvements for the projects to improve code quality and overall performance.</p>
+              </li>
+      </ul>
+    </div>
+
+    <div className="mt-14">
+      <div className="flex mt-4 text-lg text-secondaryColor ml-5 gap-16 md:gap-96 md:ml-1">
+      <h3>Mentor (Volunteer) <span className="block text-sm ml-1 text-skyColor">November 2021 – December 2021</span></h3>
+      <p><img src={microverseLogo} alt="microverse-logo" className="inline w-8"/> Microverse <span className="block text-sm text-skyColor">Remote</span></p>
+      </div>
+      <ul className="ml-4 md:ml-1">
+      <li className="flex text-base gap-2 text-skyColor mt-4">
+                <ChevronRightIcon className="h-6" />
+                <p>Mentored junior web developers, providing technical support through code reviews.</p>
+              </li>
+              <li className="flex text-base gap-2 text-skyColor mt-2">
+                <ChevronRightIcon className="h-6" />
+                <p>Provided advice and tips on how to maintain motivation to maintain longevity in the program.</p>
+              </li>
+      </ul>
+    </div>
+
+    <div className="mt-14">
+      <div className="flex mt-4 text-lg text-secondaryColor ml-5 gap-16 md:gap-96 md:ml-2">
+      <h3>Freelance Developer<span className="block text-sm ml-1 text-skyColor">January 2020 – September 2021 &#160; &#160;</span></h3>
+      <p><img src={freelanceLogo} alt="microverse-logo" className="inline w-8"/> Freelance <span className="block text-sm text-skyColor">Remote</span></p>
+      </div>
+      <ul className="ml-4 md:ml-2">
+      <li className="flex text-base gap-2 text-skyColor mt-4">
+                <ChevronRightIcon className="h-6" />
+                <p>Mentored junior web developers, providing technical support through code reviews.</p>
+              </li>
+              <li className="flex text-base gap-2 text-skyColor mt-2">
+                <ChevronRightIcon className="h-6" />
+                <p>Provided advice and tips on how to maintain motivation to maintain longevity in the program.</p>
               </li>
       </ul>
     </div>
