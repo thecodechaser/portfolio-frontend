52a5e84b6de81b0a8edc08955df6ddd67e2aa35a
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-22 23:56:06 +0530

    Install font awsome

--- src/components/Portfolio/Project.js
+++ src/components/Portfolio/Project.js
@@ -14,14 +14,17 @@ const Project = () => {
           Lorem Ipsum is simply dummy text of the printing and typesetting industry.
           Lorem Ipsum has been the industry standard dummy text ever since the 1500s,
           when an unknown printer took a galley of type and scrambled it to make a type
-          specimen book. It has survived not only five centuries, but also the leap into
-          electronic typesetting,
+          specimen book.
         </p>
         <div className={`flex ${even ? 'md:justify-start' : 'md:justify-end'} gap-2 text-skyColor mt-4 ml-6 md:ml-0`}>
           <p>HTML</p>
           <p>CSS</p>
           <p>JavaScript</p>
           <p>Ruby</p>
+        </div>
+        <div className={`flex ${even ? 'md:justify-start' : 'md:justify-end'} gap-10 text-skyColor mt-4 ml-6 md:ml-0`}>
+          <button className="bg-secondaryColor rounded px-6 py-2 w-50 text-md text-primaryColor">See Live</button>
+          <button className="bg-secondaryColor rounded px-6 py-2 w-50 text-md text-primaryColor">See Source<FontAwesomeIcon icon="fa-brands fa-github" /></button>
         </div>
       </div>
     </div>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,7 +2,7 @@ import Project from "../components/Portfolio/Project";
 
 const Portfolio = () => {
   return (
-    <div className="mt-14 md:mt-24">
+    <div className="mt-20 md:mt-40">
       <div className="flex gap-3 ml-5 md:ml-0 mb-6">
         <h2 className="text-2xl md:text-4xl">Portfolio</h2>
         <div className="border-b-2 w-4/6 mb-3 border-secondaryColor hr-portfolio" />
