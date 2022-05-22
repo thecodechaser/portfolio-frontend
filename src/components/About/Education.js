579feed1ca6b3643f28bf9628eb306d166ecc216
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-22 20:44:07 +0530

    Add college in education

--- src/components/About/Education.js
+++ src/components/About/Education.js
@@ -1,4 +1,5 @@
 import microverseLogo from '../../assets/microverse_logo.png';
+import tmuLogo from '../../assets/tmu-logo.png';
 import { ChevronRightIcon } from '@heroicons/react/solid';
 
 const Education = () => {
@@ -35,6 +36,36 @@ const Education = () => {
           <p>
           Developed skills in remote pair-programming using GitHub, industry-standard git-flow, and daily 
           standups to communicate and collaborate with international remote developers.
+          </p>
+        </li>
+      </ul>
+    </div>
+
+    <div className="mt-8 ml-1 md:ml-3">
+      <div className="flex mt-4 text-lg text-secondaryColor ml-5 gap-5 edu-tmu md:ml-0">
+        <h3>
+        Diploma Polytechnic, Full-Time
+          <span className="block text-sm ml-1 text-skyColor">July 2016 – June 2019</span>
+        </h3>
+        <p>
+          <img src={tmuLogo} alt="microverse-logo" className="inline w-8" />
+          {' '}
+          Teerthanker Mahaveer University
+          <span className="block text-sm text-skyColor">Regular</span>
+        </p>
+      </div>
+      <ul className="ml-4 md:ml-0">
+        <li className="flex text-base gap-2 text-skyColor mt-4 md:w-9/12">
+          <ChevronRightIcon className="h-6" />
+          <p>
+          Studied Computer science engineering for 3 years and learned about computer fundamentals 
+          and software development lifecycle.
+          </p>
+        </li>
+        <li className="flex text-base gap-2 text-skyColor mt-2 md:w-9/12">
+          <ChevronRightIcon className="h-6" />
+          <p>
+          Worked as a back-end Java developer for 3 months for the university internship.
           </p>
         </li>
       </ul>
