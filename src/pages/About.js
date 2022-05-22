255ac23e09265f80f9aab61b897ab218993bacdd
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-22 16:57:54 +0530

    Create about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,13 @@
 import AboutMe from "../components/About/AboutMe";
+import Experience from "../components/About/Experience";
 
 const About = () => {
   return (
   <div>
     {/* about-me-section */}
     <AboutMe />
-    
+    {/* experience-section */}
+    <Experience />
   </div>
   )
 }
