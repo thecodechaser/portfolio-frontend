f405bdf2dae51cfdd28a9dc12faae26b2c829ecb
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-22 16:58:37 +0530

    Create route for about

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router';
 import Header from './components/Header';
 import Home from './pages/Home';
+import About from './pages/About';
 import Footer from './components/Footer';
 import './App.css';
 
@@ -13,6 +14,10 @@ function App() {
           <Route
             path="/"
             element={(<Home />)}
+          />
+          <Route
+            path="/about"
+            element={(<About />)}
           />
         </Routes>
       </div>
