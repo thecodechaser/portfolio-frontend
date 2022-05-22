ad20d278b4bfb3b1324fef8afc12b98bca021f5e
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-22 20:59:42 +0530

    Fix linters

--- src/components/About/Education.js
+++ src/components/About/Education.js
@@ -1,11 +1,10 @@
+import { ChevronRightIcon } from '@heroicons/react/solid';
 import microverseLogo from '../../assets/microverse_logo.png';
 import tmuLogo from '../../assets/tmu-logo.png';
-import bhagwantUniversity from '../../assets/bhagwant-university-logo.jpg'
-import { ChevronRightIcon } from '@heroicons/react/solid';
+import bhagwantUniversity from '../../assets/bhagwant-university-logo.jpg';
 
-const Education = () => {
-  return (
-    <div className="mt-14 md:mt-24 md:ml-3">
+const Education = () => (
+  <div className="mt-14 md:mt-24 md:ml-3">
     <div className="flex gap-3 ml-4 md:ml-0">
       <h2 className="text-2xl md:text-4xl">Education</h2>
       <div className="border-b-2 w-3/5 mb-3 border-secondaryColor hr-about" />
@@ -14,7 +13,7 @@ const Education = () => {
     <div className="mt-8 md:ml-2">
       <div className="flex mt-4 text-lg text-secondaryColor ml-5 gap-5 md:gap-52 md:ml-0">
         <h3>
-        Full-stack Web Development Program, Full Time
+          Full-stack Web Development Program, Full Time
           <span className="block text-sm ml-1 text-skyColor">October 2021 – May 2022</span>
         </h3>
         <p>
@@ -28,15 +27,17 @@ const Education = () => {
         <li className="flex text-base gap-2 text-skyColor mt-4 md:w-9/12">
           <ChevronRightIcon className="h-6" />
           <p>
-          Spent 1300+ hours mastering algorithms, data structures, and full-stack development while 
-          simultaneously developing projects with Ruby, Rails, JavaScript, React, and Redux.
+            Spent 1300+ hours mastering algorithms, data structures, and
+            full-stack development while simultaneously developing projects
+            with Ruby, Rails, JavaScript, React, and Redux.
           </p>
         </li>
         <li className="flex text-base gap-2 text-skyColor mt-2 md:w-9/12">
           <ChevronRightIcon className="h-6" />
           <p>
-          Developed skills in remote pair-programming using GitHub, industry-standard git-flow, and daily 
-          standups to communicate and collaborate with international remote developers.
+            Developed skills in remote pair-programming using GitHub,
+            industry-standard git-flow, and daily standups to communicate
+            and collaborate with international remote developers.
           </p>
         </li>
       </ul>
@@ -45,7 +46,7 @@ const Education = () => {
     <div className="mt-8 ml-1 md:ml-3">
       <div className="flex mt-4 text-lg text-secondaryColor ml-5 gap-5 edu-tmu md:ml-0">
         <h3>
-        Diploma Polytechnic, Full-Time
+          Diploma Polytechnic, Full-Time
           <span className="block text-sm ml-1 text-skyColor">July 2016 – June 2019</span>
         </h3>
         <p>
@@ -59,14 +60,14 @@ const Education = () => {
         <li className="flex text-base gap-2 text-skyColor mt-4 md:w-9/12">
           <ChevronRightIcon className="h-6" />
           <p>
-          Studied Computer science engineering for 3 years and learned about computer fundamentals 
-          and software development lifecycle.
+            Studied Computer science engineering for 3 years and learned about computer fundamentals
+            and software development lifecycle.
           </p>
         </li>
         <li className="flex text-base gap-2 text-skyColor mt-2 md:w-9/12">
           <ChevronRightIcon className="h-6" />
           <p>
-          Worked as a back-end Java developer for 3 months for the university internship.
+            Worked as a back-end Java developer for 3 months for the university internship.
           </p>
         </li>
       </ul>
@@ -75,7 +76,7 @@ const Education = () => {
     <div className="mt-8 ml-2 md:ml-4">
       <div className="flex mt-4 text-lg text-secondaryColor ml-5 gap-5 edu-bhag edu-tmu md:ml-0">
         <h3>
-        Bachlor of computer science
+          Bachlor of computer science
           <span className="block text-sm ml-1 text-skyColor">July 2021 – Present</span>
         </h3>
         <p>
@@ -89,15 +90,14 @@ const Education = () => {
         <li className="flex text-base gap-2 text-skyColor mt-4 md:w-9/12">
           <ChevronRightIcon className="h-6" />
           <p>
-          Studying Computer science engineering for 4 years and learning about computer fundamentals 
-          and software development lifecycle.
+            Studying Computer science engineering for 4 years and learning
+            about computer fundamentals and software development lifecycle.
           </p>
         </li>
       </ul>
     </div>
 
-    </div>
-  )
-}
+  </div>
+);
 
 export default Education;
