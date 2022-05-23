3fa129a8f32dc8684fe762081acd2906ddfd617e
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-23 17:11:32 +0530

    Work on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="flex justify-center my-5">
+  <footer className="flex flex-col items-center my-8">
+    <button type="button" className="bg-secondaryColor rounded px-4 my-10 py-2"><i class="las la-angle-up"></i></button>
     <p className="text-md">
       Build by
       <a href="https://github.com/thecodechaser" className="text-secondaryColor"> Ranjeet Singh </a>
