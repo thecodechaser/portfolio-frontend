81475a7acee4d8ad8e13a3877b22f88fd888533d
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-24 15:02:11 +0530

    Applys styles

--- src/components/Resume/ResumePDF.js
+++ src/components/Resume/ResumePDF.js
@@ -7,7 +7,7 @@ const ResumePDF =()=>{
 <object data={ranjeetPDF} type="application/pdf" className="w-full pdf">
       
   </object>
-  <button type="submit" className="bg-secondaryColor mt-5 ml-2 md:ml-0 rounded px-4 py-3 text-base text-primaryColor">Download Resume</button>
+  <button type="submit" className="bg-secondaryColor mt-5 ml-2 rounded px-4 py-3 text-base text-primaryColor">Download Resume</button>
     </div>
   )
 }
