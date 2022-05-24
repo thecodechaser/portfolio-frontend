09d6a1196ba92c35ff4694cc5e43cd3a0253c059
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-24 14:42:43 +0530

    Create Resume component

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,6 +1,12 @@
 const Resume = ()=>{
   return (
-    <p>Hello</p>
+    <div className="mt-32">
+    <div className="flex gap-3 ml-5 md:ml-0 mb-6">
+      <h2 className="text-2xl md:text-4xl">Resume</h2>
+      <div className="border-b-2 w-4/6 mb-3 border-secondaryColor hr-portfolio" />
+    </div>
+  </div>
+    
   )
 }
 
