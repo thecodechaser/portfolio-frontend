dd827ae8b870b29399b866e768d001dc47eab2cc
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-24 14:59:43 +0530

    Add resume pdf

--- src/components/Resume/ResumePDF.js
+++ src/components/Resume/ResumePDF.js
@@ -1,6 +1,13 @@
+import ranjeetPDF from '../../assets/data/Ranjeet-Resume.pdf';
+
 const ResumePDF =()=>{
   return (
-    <p>Hello</p>
+    <div>
+
+<object data={ranjeetPDF} type="application/pdf" className="w-full pdf">
+      
+  </object>
+    </div>
   )
 }
 
