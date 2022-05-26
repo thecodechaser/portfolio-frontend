fb51b482fcc2bb11ff7774ca22b2c01ed4b7c355
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-26 13:49:00 +0530

    Add styles to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,13 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () =>{
   return (
-    <section className="flex flex-col items-center mt-32 md:mt-40">
+    <section className="flex flex-col items-center mt-60 md:mr-52">
       <p className="text-3xl md:text-5xl">Lost your way?</p>
       <p className="text-lg mt-5">
         Sorry, we can&apos;t find that page.
       </p>
-      <Link to="/">Go Home</Link>
+      <Link to="/"><button type="submit" className="bg-secondaryColor mt-5 ml-2 rounded px-4 py-3 text-base 
+      text-primaryColor">Go Home</button></Link>
   </section>
   )
 }
