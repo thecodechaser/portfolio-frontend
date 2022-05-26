dde5f197a584deb69972d8cf198af1d39acfe348
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-27 00:33:08 +0530

    Apply styles

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,8 +1,22 @@
 import { Link } from 'react-router-dom';
+import rocketGif from '../../assets/rocket.gif'
 
 const Blog = () => {
   return (
-    <p>Hello</p>
+    <section className="flex flex-col items-center mt-10 md:mr-52">
+    <p className="text-xl ml-4 md:ml-0">I'm a very intresting feature and currenty under development.</p>
+    <p className="text-lg ml-4 mt-4 md:ml-0">Next time you come back I will be live here.</p>
+    <img src={rocketGif} alt="rocket-gif" />
+    <Link to="/">
+      <button
+        type="submit"
+        className="bg-secondaryColor mt-5 ml-2 rounded px-4 py-3 text-base
+      text-primaryColor"
+      >
+        Go Home
+      </button>
+    </Link>
+  </section>
   )
 }
 
