21cb878097dd23155f187e9612dfb00438f74ea3
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-26 23:40:44 +0530

    Add blog heading

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,6 +1,11 @@
 const Blog = () => {
   return (
-    <p>Hello</p>
+    <div className="mt-32 md:mt-40">
+    <div className="flex gap-3 ml-5 md:ml-0 mb-6">
+      <h2 className="text-2xl md:text-4xl">Latest Blogs</h2>
+      <div className="border-b-2 mb-3 border-secondaryColor hr-blog" />
+    </div>
+  </div>
   )
 }
 
