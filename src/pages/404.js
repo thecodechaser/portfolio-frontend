049d31499f6a93fa97aa066be269b879e3a61f58
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-26 13:46:55 +0530

    Add 404 page content

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,17 +2,12 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () =>{
   return (
-    <section className="not-found mt-32 md:mt-40">
-    <div>
-      <p>Lost your way?</p>
-      <span>
-        Sorry, we can&apos;t find that page. You can find doctors in the
-        homepage and start booking appointments
-      </span>
-    </div>
-    <div>
+    <section className="flex flex-col items-center mt-32 md:mt-40">
+      <p className="text-3xl md:text-5xl">Lost your way?</p>
+      <p className="text-lg mt-5">
+        Sorry, we can&apos;t find that page.
+      </p>
       <Link to="/">Go Home</Link>
-    </div>
   </section>
   )
 }
