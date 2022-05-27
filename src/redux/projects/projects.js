3b7f16a75200c860c6ec792a0cfac4adab3a6f7d
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-27 20:25:17 +0530

    Add more projects

--- src/redux/projects/projects.js
+++ src/redux/projects/projects.js
@@ -19,6 +19,76 @@ const initialState = [
       'Rails', 'React', 'Tailwind',
     ],
   },
+  {
+    title: 'Covid19 Metrics',
+    image: covid19Metrics,
+    details: 'Covid19 Metrics is a web application that fetches covid data from API. It shows all countries with the confirmed covid cases on the homepage, users can select any country and go to the details page. The details page shows other details for that specific country like recovered cases, confirmed cases, and deaths.',
+    live: 'https://covid19-metrics-thecodechaser.netlify.app/',
+    github: 'https://github.com/thecodechaser/covid19-metrics',
+    tech: [
+      'React', 'Redux', 'HTML & CSS',
+    ],
+  },
+  {
+    title: 'Disney Characters',
+    image: disneyCharacters,
+    details: 'Disney Character is a web application that shows data of Disney characters from the Disney API. Users can like the character and users can also leave comments on any character on the comment popup. It shows the comments left by other people and gives an idea of what people think about Disney characters.',
+    live: 'https://thecodechaser.github.io/Disney-characters-webapp/dist/',
+    github: 'https://github.com/thecodechaser/Disney-characters-webapp',
+    tech: [
+      'JavaScript', 'HTML & CSS',
+    ],
+  },
+  {
+    title: 'Space Travelers',
+    image: spaceTravelers,
+    details: 'Space Travelers Hub is a React web application that fetches rockets, and missions from the SpaceX API. Users can book and cancel their reservations for rockets and missions. The application has three pages Rockets/Missions/MY Profile, My Profile page shows the list of reserved rockets and missions.',
+    live: 'https://space-travelers-thecodechaser.netlify.app/',
+    github: 'https://github.com/thecodechaser/space-travelers-hub',
+    tech: [
+      'React', 'Redux', 'HTML & CSS',
+    ],
+  },
+  {
+    title: 'Thecodechaser BootCamp',
+    image: bootCamp,
+    details: 'ThecodeChaser Bootcamp is a web development boot camp website, responsive for desktop and mobile screens. It delivers all related information and user can submit their query and questions using the mentioned form.',
+    live: 'https://thecodechaser.github.io/thecodechaser-web-bootcamp/',
+    github: 'https://github.com/thecodechaser/thecodechaser-web-bootcamp',
+    tech: [
+      'HTML', 'CSS',
+    ],
+  },
+  {
+    title: 'Recipe App',
+    image: recipeApp,
+    details: 'The Recipe app is a classic example of a recipe website. Its a functional website that shows the list of foods and recipes and empowers readers to interact with them by adding new foods and recipes. Users can access the services of the application by creating a new account or by login in if they already have an account.',
+    live: 'https://recipe-app-thecodechaser.herokuapp.com/',
+    github: 'https://github.com/thecodechaser/recipe-app',
+    tech: [
+      'Rails', 'Ruby', 'Tailwind',
+    ],
+  },
+  {
+    title: 'Book Store',
+    image: bookStore,
+    details: 'Book Store is a web application to add favorite books by the user, users can add and remove books, This website is built using REACT and JavaScript libraries.',
+    live: 'https://book-store-thecodechaser.netlify.app/',
+    github: 'https://github.com/thecodechaser/book-store',
+    tech: [
+      'React', 'Redux', 'HTML & CSS',
+    ],
+  },
+  {
+    title: 'Budget Tracker',
+    image: budgetTracker,
+    details: 'The Budget tracker app is a full-stack application. Its a functional website that shows the list of categories and transactions and empowers readers to interact with them by adding new categories and transactions. Users can access the services of the application by creating a new account or by login in if they already have an account.',
+    live: 'https://budget-tracker-thecodechaser.herokuapp.com/',
+    github: 'https://github.com/thecodechaser/budget-tracker',
+    tech: [
+      'Rails', 'Ruby', 'Tailwind',
+    ],
+  },
 ];
 
 // reducer
