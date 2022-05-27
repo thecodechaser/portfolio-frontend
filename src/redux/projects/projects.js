5f7e47c82ea385b20fb9e8c2b0a00b23d675e654
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-27 13:38:36 +0530

    Call project reducer in redux store

--- src/redux/projects/projects.js
+++ src/redux/projects/projects.js
@@ -5,8 +5,6 @@ const initialState = [];
 // reducer
 const projectsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case FETCH_DATA:
-      return { ...state, countriesData: action.Countries, globalData: action.Global };
     default:
       return state;
   }
