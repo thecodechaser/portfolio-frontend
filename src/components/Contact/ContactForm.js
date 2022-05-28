873d06321db959593867391b4db2fee9c89a7ac2
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-28 16:22:17 +0530

    Add validation to contact form

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -1,11 +1,41 @@
-const ContactForm = () => (
+import { useState } from "react";
+
+const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [successNotice, setSuccessNotice] = useState(false);
+  
+  const submission = () => {
+    const pattern = /\S+@\S+\.\S+/;
+
+    if(name === '' || email === '' || message === ''){
+      return;
+    }
+
+    if(!pattern.test(email)){
+      return false;
+    }
+
+    setSuccessNotice(true);
+    setTimeout(function(){
+      setEmail('');
+     setMessage('');
+     setName('');
+    }, 800);
+    
+  }
+
+  return (
   <div className="flex flex-col mt-10 ml-6 md:ml-0 md:items-center md:mr-52">
     <h3 className="text-xl font-bold text-skyColor ml-24 md:ml-0  mb-4">Let&apos;s Chat</h3>
-    <form className="flex flex-col gap-4">
+    <form className="flex flex-col gap-4" action="https://formspree.io/f/meqnnlpy" method="post">
       <input
         type="text"
         name="user_name"
         required
+        value={name}
+        onChange={(e)=> setName(e.target.value)}
         maxLength="30"
         placeholder="Name"
         className="w-72 md:w-80 p-2 rounded text-primaryColor
@@ -15,6 +45,8 @@ const ContactForm = () => (
         type="email"
         name="user_email"
         required
+        value={email}
+        onChange={(e)=> setEmail(e.target.value)}
         placeholder="Email"
         className="w-72 md:w-80 p-2 rounded text-primaryColor
         focus:border-2 focus:border-secondaryColor focus:outline-none"
@@ -23,14 +55,22 @@ const ContactForm = () => (
         rows="10"
         name="user_msg"
         required
+        value={message}
+        onChange={(e)=> setMessage(e.target.value)}
         maxLength="500"
         placeholder="Write your message here"
         className="w-72 md:w-80 p-2 rounded text-primaryColor
           focus:border-2 focus:border-secondaryColor focus:outline-none"
       />
-      <button type="submit" className="bg-lightBlueColor ml-24 mt-5 rounded w-28 px-4 py-2 text-base text-skyColor font-medium">Submit</button>
+      {
+        successNotice && (
+          <span className="text-skyColor">Thank you for your message, I will get back to you soon!</span>
+        )
+      }
+      <button onClick={submission} type="submit" className="bg-lightBlueColor ml-24 mt-6 rounded w-28 px-4 py-2 text-base text-skyColor font-medium">Submit</button>
     </form>
   </div>
-);
+)
+  };
 
 export default ContactForm;
