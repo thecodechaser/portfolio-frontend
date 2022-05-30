87351186891a87476ce3ea0d6f2de2fcdc094255
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-30 15:49:23 +0530

    Update footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -25,9 +25,9 @@ const Footer = () => (
         <i className="las la-angle-up" />
       </button>
       <p className="text-md">
-        Build by
+        Copyright © 2022
         <a href="https://github.com/thecodechaser" className="text-secondaryColor"> Ranjeet Singh </a>
-        © 2022
+        All Rights Reserved
       </p>
     </footer>
   </>
