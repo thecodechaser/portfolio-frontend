5a1ee79708b4b567329fa6df79a3c8ab916df9eb
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-05-30 15:42:38 +0530

    Add animations to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,13 @@
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const NotFound = () => (
-  <section className="flex flex-col items-center mt-60 md:mr-52">
+  <motion.section
+    initial={{ x: '-100vw' }}
+    animate={{ x: -0 }}
+    transition={{ duration: 1 }}
+    className="flex flex-col items-center mt-60 md:mr-52"
+  >
     <p className="text-3xl md:text-5xl">Lost your way?</p>
     <p className="text-lg mt-5">
       Sorry, we can&apos;t find that page.
@@ -15,7 +21,7 @@ const NotFound = () => (
         Go Home
       </button>
     </Link>
-  </section>
+  </motion.section>
 );
 
 export default NotFound;
