056601e274d9cda8d5422c0ef08b261f8e870b70
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-07 07:30:10 +0530

    Call all reducers in store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,9 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import projectsReducer from './projects/projects';
+import postsReducer from './blogs/posts';
+import commentsReducer from './blogs/comments';
+import likesReducer from './blogs/likes';
 
 const reducer = combineReducers({
-  projectsReducer,
+  projectsReducer, postsReducer, commentsReducer, likesReducer,
 });
 
 const store = createStore(
