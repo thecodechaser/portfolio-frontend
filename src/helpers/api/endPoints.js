40bdd0c54c9ee9a37e95ba8ea4de60a2d7dd3f24
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-07 07:40:31 +0530

    Add helpers for endpoints

--- src/helpers/api/endPoints.js
+++ src/helpers/api/endPoints.js
@@ -1,1 +1,8 @@
 const BASE_URL = 'https://localhost:3000/api/v1';
+const POST_URL = `${BASE_URL}/posts`
+const COMMENT_URL = `${BASE_URL}/comments`
+const LIKE_URL = `${BASE_URL}/likes`
+
+const API_TOKEN = 'Hiyzj5VbF9AdZ2_Mu5hz';
+
+export {API_TOKEN, POST_URL, COMMENT_URL, LIKE_URL}
