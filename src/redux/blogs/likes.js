0588e406de92b6fff39ce5b28bda6332820f5b9a
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-07 07:28:01 +0530

    Add comments reducer

--- src/redux/blogs/likes.js
+++ src/redux/blogs/likes.js
@@ -1,2 +1,12 @@
 // initial-state
 const initialState = []
+
+// reducer
+const commentsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default commentsReducer;
