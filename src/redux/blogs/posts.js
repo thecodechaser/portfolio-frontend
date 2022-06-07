edbb7e0e0a76935c3fa43b67fe5c80e5a6dfa49f
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-07 08:21:08 +0530

    Update postsReducer

--- src/redux/blogs/posts.js
+++ src/redux/blogs/posts.js
@@ -26,6 +26,8 @@ export const fetchPostsApi = () => async (dispatch) => {
 // reducer
 const postsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_POSTS:
+      return action.payload;
     default:
       return state;
   }
