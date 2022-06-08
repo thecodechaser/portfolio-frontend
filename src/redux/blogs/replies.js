4ec2e9341e39a0e6353da6bf9b78baa109fc4c97
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-08 11:25:26 +0530

    Add replies reducer

---
+++ src/redux/blogs/replies.js
@@ -0,0 +1,12 @@
+// initial-state
+const initialState = []
+
+// reducer
+const repliesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default repliesReducer;
