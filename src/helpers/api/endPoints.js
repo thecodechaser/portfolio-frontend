c0bdfe34ac4d374905e96b8aeb01932076c8adb6
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-08 11:50:21 +0530

    Add api url for replies

--- src/helpers/api/endPoints.js
+++ src/helpers/api/endPoints.js
@@ -1,8 +1,9 @@
 const BASE_URL = 'http://localhost:3000/api/v1';
 const POST_URL = `${BASE_URL}/posts`
 const COMMENT_URL = `${BASE_URL}/comments`
+const REPLY_URL = `${BASE_URL}/replies`
 const LIKE_URL = `${BASE_URL}/likes`
 
 const API_TOKEN = 'Hiyzj5VbF9AdZ2_Mu5hz';
 
-export {API_TOKEN, POST_URL, COMMENT_URL, LIKE_URL}
+export {API_TOKEN, POST_URL, COMMENT_URL, REPLY_URL, LIKE_URL}
