f2a5d7372540a3c86cfc943900f620d1421c3bed
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-08 14:08:13 +0530

    Create blog card

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -11,21 +11,24 @@ const Blog = (props) => {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ delay: 0.1, duration: 0.8 }}
-      className={`md:flex project-card ${data.id === 9 ? 'mt-16' : 'mt-32'} mx-2 ml-4 md:ml-0 md:mx-0 p-3 rounded-md shadow shadow-lightBlueColor md:gap-5`}
+      className={`md:flex project-card ${data.id == 9 ? 'mt-16' : 'mt-32'} mx-2 ml-4 md:ml-0 md:mx-0 p-3 rounded-md shadow shadow-lightBlueColor md:gap-5`}
     >
+      <div className="w-11/12 post-img">
+      <Link to="/" >
       <img
         src={data.photo_one}
         alt="project preview"
-        className="w-11/12 ml-2 rounded-md card-img md:ml-0 "
+        className=" ml-2 rounded-md  md:ml-0 "
       />
+      </Link></div>
       <div>
-        <h3 className="text-skyColor mt-3 md:mt-0 mb-4 text-2xl ml-2 md:ml-0">{data.title}</h3>
+      <Link to="/"><h3 className="text-skyColor mt-3 md:mt-0 mb-4 text-xl md:text-2xl ml-2 md:ml-0">{data.title}</h3></Link>
         <p className="bg-lightBlueColor text-skyColor text-base font-medium p-5 rounded mr-6 ml-2 md:ml-0 md:mr-0">
           {data.p_one.substring(0, 200)}
           ....
         </p>
         <div className="flex justify-between gap-2 text-skyColor mt-8 ml-2 md:ml-0">
-          <div className="flex gap-2 ml-1 mt-2">
+          <div className="flex gap-3 ml-1 mt-2">
             <p>
               {data.likes_counter}
               {' '}
@@ -37,10 +40,11 @@ const Blog = (props) => {
               Comments
             </p>
           </div>
+          <Link to="/">
           <button type="button" className="bg-lightBlueColor mr-8 md:mr-0 rounded px-3 py-2 text-md text-skyColor font-medium hover:bg-secondaryColor hover:text-primaryColor">
             Read More
             <ArrowRightIcon className="h-4 inline ml-1" />
-          </button>
+          </button></Link>
         </div>
 
       </div>
