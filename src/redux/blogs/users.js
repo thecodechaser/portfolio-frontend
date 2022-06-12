5b221d2b8bc2a6c9762155bdb9a257ebe19a4dd6
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-12 12:16:07 +0530

    Add const state in users

--- src/redux/blogs/users.js
+++ src/redux/blogs/users.js
@@ -1,9 +1,34 @@
+import Axios from 'axios';
+import { API_TOKEN, USER_URL } from '../../helpers/api/endPoints';
+
+// constants
+const FETCH_USERS = 'PORTFOLIO/BLOGS/USERS';
+
+// actions
+const fetchUsers = (payload) => ({
+  type: FETCH_USERS,
+  payload,
+});
+
 // initial-state
 const initialState = [];
+
+// API-functions
+export const fetchPostsApi = () => async (dispatch) => {
+  const returnValue = await Axios.get(`${POST_URL}/load`, {
+    headers: {
+      Authorization: API_TOKEN,
+    },
+  });
+  const { posts } = returnValue.data.data;
+  dispatch(fetchPosts(posts));
+};
 
 // reducer
 const usersReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_USERS:
+      return action.payload;
     default:
       return state;
   }
