83621c86d432e54622407871ca6ad6633ba24a81
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-12 12:12:01 +0530

    Add reducer for users

---
+++ src/redux/blogs/users.js
@@ -0,0 +1,12 @@
+// initial-state
+const initialState = [];
+
+// reducer
+const usersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default usersReducer;
