7c75181aa76c45efa383d2b193a114dc9580a4df
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-12 12:12:27 +0530

    Add users reducer in store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,11 +4,12 @@ import thunk from 'redux-thunk';
 import projectsReducer from './projects/projects';
 import postsReducer from './blogs/posts';
 import commentsReducer from './blogs/comments';
+import usersReducer from './blogs/users';
 import repliesReducer from './blogs/replies';
 import likesReducer from './blogs/likes';
 
 const reducer = combineReducers({
-  projectsReducer, postsReducer, commentsReducer, repliesReducer, likesReducer,
+  projectsReducer, postsReducer, commentsReducer, repliesReducer, likesReducer, usersReducer
 });
 
 const store = createStore(
