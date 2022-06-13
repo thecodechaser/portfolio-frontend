ed050627abdd89841932f7f70b20b89acd7c9b13
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-13 12:46:43 +0530

    Add const state for create likes

--- src/redux/blogs/likes.js
+++ src/redux/blogs/likes.js
@@ -2,11 +2,17 @@ import Axios from 'axios';
 import { API_TOKEN, LIKE_URL } from '../../helpers/api/endPoints';
 
 // constants
-const FETCH_LIKES = 'PORTFOLIO/BLOGS/LIKES';
+const FETCH_LIKES = 'PORTFOLIO/BLOGS/LIKES/LOAD';
+const CREATE_LIKES = 'PORTFOLIO/BLOGS/LIKES/CREATE';
 
 // actions
 const fetchLikes = (payload) => ({
   type: FETCH_LIKES,
+  payload,
+});
+
+const createLikes = (payload) => ({
+  type: CREATE_LIKES,
   payload,
 });
 
@@ -24,6 +30,16 @@ export const fetchLikesApi = (data) => async (dispatch) => {
   const { likes } = returnValue.data.data;
   dispatch(fetchLikes(likes));
 };
+
+export const createLikeApi = (data) => async (dispatch) => {
+  const returnValue = await Axios.post(`${LIKE_URL}/create`, data, {
+    headers: {
+      Authorization: API_TOKEN,
+    },
+  });
+  const { like } = returnValue.data.data;
+  dispatch(fetchLikes(like));
+}
 
 // reducer
 const likesReducer = (state = initialState, action) => {
