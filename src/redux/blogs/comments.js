f3396de369e95eaf844ade6f1ea29ce09f16f255
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-16 13:38:34 +0530

    Update reducer

--- src/redux/blogs/comments.js
+++ src/redux/blogs/comments.js
@@ -33,6 +33,8 @@ export const fetchCommentsApi = (data) => async (dispatch) => {
 // reducer
 const commentsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_COMMENTS:
+      return action.payload;
     default:
       return state;
   }
