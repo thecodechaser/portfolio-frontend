ce475d5337e7228017399610daeedf2d1e4448c4
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-16 14:33:05 +0530

    Add form inputs

--- src/components/Posts/CommentForm.js
+++ src/components/Posts/CommentForm.js
@@ -1,6 +1,29 @@
 const CommentForm = () => {
   return (
-    <p>Hello</p>
+    <div>
+       <input
+          type="text"
+          name="user_name"
+          required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          maxLength="30"
+          placeholder="Name"
+          className="w-72 md:w-80 p-2 rounded text-primaryColor
+      focus:border-2 focus:border-secondaryColor focus:outline-none"
+        />
+         <textarea
+          rows="10"
+          name="user_msg"
+          required
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          maxLength="500"
+          placeholder="Write your message here"
+          className="w-72 md:w-80 p-2 rounded text-primaryColor
+          focus:border-2 focus:border-secondaryColor focus:outline-none"
+        />
+    </div>
   )
 };
 
