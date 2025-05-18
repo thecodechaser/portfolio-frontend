c7906f61dbbb8bd8d64a81379ff40170b817e680
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2025-05-18 17:05:13 +0530

    Update RESUME

--- src/components/Resume/ResumePDF.js
+++ src/components/Resume/ResumePDF.js
@@ -4,7 +4,7 @@ const ResumePDF = () => (
   <div>
     <object aria-label="resume-pdF" data={ranjeetPDF} type="application/pdf" className="w-full pdf" />
     <button
-      onClick={() => window.open('https://drive.google.com/file/d/1DG_9WxaGaNy79K0FkAxMZzhjvwjXbIRF/view?usp=sharing', '_blank')}
+      onClick={() => window.open('https://drive.google.com/file/d/1x5toIxCBfJvwBFeKqj55CRzaDjW_9gYX/view?usp=sharing', '_blank')}
       type="submit"
       className="px-4 py-3 mt-5 ml-2 text-lg font-medium rounded bg-lightBlueColor text-skyColor hover:bg-secondaryColor hover:text-primaryColor"
     >
